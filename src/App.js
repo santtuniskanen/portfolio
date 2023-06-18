dfbb34e49f40f5ed18e13e097e13691d3f6a1284
santtuniskanen <santtuniska02@gmail.com>
2023-06-18 12:41:50 +0300

    starting to figure out flexbox

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Santtu Niskanen</h1>
-      &nbsp;
-      <p>I am a 20-year-old Software Engineering student from Oulu, Finland. I have an interest towards DevOps.</p>
-      &nbsp;
-      <p>Hello hello</p>
+    <div className="flexbox-container">
+        <div class="flexbox-item flexbox-item-1">
+          <h1>A</h1>
+          </div> 
+        <div class="flexbox-item flexbox-item-2">
+          <h1>mo</h1>
+          </div> 
+        <div class="flexbox-item flexbox-item-3">
+          <h1>gus</h1>
+          </div> 
     </div>
   );
 }
