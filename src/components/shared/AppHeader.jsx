4ff8f9cb0a65baaf9d1239abeb5f6fbff15a259e
santtuniskanen <santtuniska02@gmail.com>
2023-07-22 14:50:45 +0300

    a lot has changed

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -1,7 +1,18 @@
 import logo from '../../images/Lizard-Silhouette.svg';
 
-return (
+const AppHeader= () => {
 
-)
+  return (
+    <div className="flex justify-between items-center px-4 sm:px-0">
+      <div>
+        <img
+        src={logo}
+        className="w-36"
+        alt="logo salamander"
+        />
+      </div>
+    </div>
+  );
+}
 
 export default AppHeader;
