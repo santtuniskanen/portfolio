e8d75af5a5dc359d88a9471c9f16505068aad08d
santtuniskanen <santtuniska02@gmail.com>
2023-07-24 12:41:56 +0300

    about section done

--- components/About.jsx
+++ components/About.jsx
@@ -13,14 +13,14 @@ const About = () => {
             </p>
             <h2 className='py-4'>Who I Am</h2>
             <p className='py-2 text-gray-600'>
-
+            I started growing an interest to computers pretty early on in my life. The fascination eventually grew big enough that I had to build my own computer and I insisted to my parents that I'd do all of it myself. That interest eventually led me to different technology related communities where I met some wonderful people who introduced me to the world of Software Engineering.
             </p>
             <p className='py-2 text-gray-600'>
-
+            I wasn't quite sure what I wanted to do after high school, but through different hoops I'd find my way to <span className='font-bold'>Oulu University of Applied Sciences</span>, studying Information Technology. On my way to the Sophomore year, I've learned electronics with our Arduino project, desktop application development, RESTAPIs and Databases with the Bank Simulator project. Going forward I'm going to bring our school projects and my interest towards <span className='font-bold'>DevOps</span> together creating CI/CD pipelines, working with containers and orchestration, working with cloud services and automation various aspects of the projects.
             </p>
             <Link href='/#projects'>
               <p className='py-2 text-gray-600 underline cursor-pointer'>
-                Check out some of my latest projects.
+                Check out what I've been building 
               </p>
             </Link>
           </div>
