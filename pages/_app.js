6a69044fe7bc86a847314366359a9234bcce0585
santtuniskanen <santtuniska02@gmail.com>
2023-07-24 17:52:05 +0300

    new stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,5 @@
+import '@/styles/globals.css'
 
-import Image from 'next/image'
-import Head from 'next/head'
-import '../styles/globals.css'
-import Navbar from '../components/Navbar.jsx'
-import Main from '../components/Main.jsx'
-import About from '../components/About.jsx'
-import Skills from '../components/Skills.jsx'
-import Projects from '../components/Projects.jsx'
-import Contact from '@/components/Contact'
-
-export default function Home() {
-    return (
-      <div>
-        <Head>
-          <title>Santtu Niskanen - Software Engineer</title>
-        </Head>
-        <Navbar />
-        <Main/>
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </div>
-    )
-  }
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
