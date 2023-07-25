097a3034a8fae9327ef312e023c848b81ecd9c8a
santtuniskanen <santtuniska02@gmail.com>
2023-07-25 22:41:41 +0300

    last commit of the day

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { Document } from "react-pdf";
 
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
       <div className="max-v-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div>
+        <div className="">
           <p className="uppercase text-sm tracking-widest text-gray-600">
             LET&apos;S GO ON A JOURNEY
           </p>
@@ -17,7 +16,7 @@ const Main = () => {
             </span>
           </h1>
           <h1 className="py-4 text-gray-700">A Software Engineer</h1>
-          <p className="py-4 text-gray-600 max-w-[75%] m-auto">
+          <p className="py-4 text-gray-600 max-w-[75%] m-auto ">
             I&apos;m a software engineer interested in building and delivering
             high performance applications. I&apos;m currently focusing on DevOps
             technologies like containerisation, deployment, infrastructure and
@@ -27,14 +26,6 @@ const Main = () => {
             <div className="shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-gradient-to-b from-sky-400 to-sky-200">
               <a href="https://linkedin.com/in/santtuniskanen">
                 <FaLinkedin />
-              </a>
-            </div>
-            <div className="shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-gradient-to-b from-orange-500 to-yellow-300">
-              <a
-                href="..public/assets/resume/santtuniskanen.pdf"
-                download
-              >
-                <p className="text-bold text-gray-800">Resume</p>
               </a>
             </div>
             <div className="shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-gradient-to-b from-orange-500 to-yellow-300">
