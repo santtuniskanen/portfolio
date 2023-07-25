3c44249b9bff2132cc2e93938b10d95e8310fb62
santtuniskanen <santtuniska02@gmail.com>
2023-07-25 15:42:31 +0300

    ddddccc

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,16 +10,15 @@ import ContactImg from "../public/assets/contact.jpg";
 
 const Contact = () => {
   return (
-    <div id="contact" className="w-full lg:h-screen">
+    <div id="contact" className="w-full flex flex-cols-3 lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Contact
         </p>
         <h2 className="py-4">Get In Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
-          {/* left */}
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
-            <div className="lg:p-4 h-full ">
+            <div className="lg:p-4 h-full">
               <div>
                 <Image
                   className="rounded-xl hover:scale-105 ease-in duration-300"
@@ -29,27 +28,25 @@ const Contact = () => {
                 />
               </div>
               <div>
-                <h2 className="py-6">Santtu Niskanen</h2>
-                <p>Software Engineer</p>
-                <p className="py-3">
+                <h2 className="py-6 text-center">Santtu Niskanen</h2>
+                <p className="text-center italic ">Software Engineer</p>
+                <p className="py-3 text-center">
                   I'm available for internship opportunies for Software Engineer
                   positions. Interested? Let's talk!
                 </p>
               </div>
               <div>
-                <p className="uppercase pt-8">Connect With Me</p>
-                <div className="flex items-center justify-between py-4">
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <FaLinkedin />
+                <p className="uppercase pt-8 text-center">Connect With Me</p>
+                <div className="flex items-center justify-center py-4 gap-8 ">
+                  <div className="shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-gradient-to-b from-sky-400 to-sky-200">
+                    <a href="https://linkedin.com/in/santtuniskanen">
+                      <FaLinkedin />
+                    </a>
                   </div>
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <FaGithub />
-                  </div>
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <AiOutlineMail />
-                  </div>
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <BsFillPersonLinesFill />
+                  <div className="shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-gradient-to-b from-orange-500 to-yellow-300">
+                    <a href="https://github.com/santtuniskanen">
+                      <FaGithub />
+                    </a>
                   </div>
                 </div>
               </div>
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,35 +1,46 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import {BsFillPersonLinesFill} from 'react-icons/bs'
-import {AiOutlineMail} from 'react-icons/ai'
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import Link from "next/link";
 
 const Main = () => {
-    return (
-        <div id='home' className="w-full h-screen text-center">
-            <div className="max-v-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-                <div>
-                    <p className="uppercase text-sm tracking-widest text-gray-600">LET's GO ON A JOURNEY</p>
-                    <h1 className="py-4 text-gray-700">Hi, I'm <span className="text-white bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% hover:from-pink-500 hover:to-yellow-500">Santtu</span></h1>
-                    <h1 className="py-4 text-gray-700">A Software Engineer</h1>
-                    <p className="py-4 text-gray-600 max-w-[75%] m-auto">I'm a software engineer interested in building and delivering high performance applications. I'm currently focusing on DevOps technologies like containerisation, deployment, infrastructure and automation.</p>
-                    <div className="flex items-center justify-between max-w-[330px] m-auto p-5">
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <FaLinkedin />
-                        </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <FaGithub />
-                        </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <BsFillPersonLinesFill />
-                        </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <AiOutlineMail />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div id="home" className="w-full h-screen text-center">
+      <div className="max-v-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LET's GO ON A JOURNEY
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm{" "}
+            <span className="text-white bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% hover:from-pink-500 hover:to-yellow-500">
+              Santtu
+            </span>
+          </h1>
+          <h1 className="py-4 text-gray-700">A Software Engineer</h1>
+          <p className="py-4 text-gray-600 max-w-[75%] m-auto">
+            I'm a software engineer interested in building and delivering high
+            performance applications. I'm currently focusing on DevOps
+            technologies like containerisation, deployment, infrastructure and
+            automation.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto p-5">
+            <div className="shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-gradient-to-b from-sky-400 to-sky-200">
+              <a href="https://linkedin.com/in/santtuniskanen">
+                <FaLinkedin />
+              </a>
             </div>
+            <div className="shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-gradient-to-b from-orange-500 to-yellow-300">
+              <a href="https://github.com/santtuniskanen">
+                <FaGithub />
+              </a>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Main;
