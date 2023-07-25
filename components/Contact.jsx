249afa0ae9a22944d954cff4796152309691325f
santtuniskanen <santtuniska02@gmail.com>
2023-07-25 13:27:10 +0300

    neeeew

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
@@ -10,14 +11,14 @@ import ContactImg from "../public/assets/contact.jpg";
 const Contact = () => {
   return (
     <div id="contact" className="w-full lg:h-screen">
-      <div className="max-w-[1240px] m-auto px-2 py-16 w-full ">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Contact
         </p>
         <h2 className="py-4">Get In Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
           {/* left */}
-          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4 border-2 border-black">
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full ">
               <div>
                 <Image
@@ -28,9 +29,9 @@ const Contact = () => {
                 />
               </div>
               <div>
-                <h2 className="py-2">Santtu Niskanen</h2>
+                <h2 className="py-6">Santtu Niskanen</h2>
                 <p>Software Engineer</p>
-                <p className="py-4">
+                <p className="py-3">
                   I'm available for internship opportunies for Software Engineer
                   positions. Interested? Let's talk!
                 </p>
